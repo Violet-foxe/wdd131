404b97d1b7e7239968d95d6fe22a5f080fa71108
BloodyBecauseYes <kaleiss.brugger@gmail.com>
2026-05-13 10:15:26 -0600

    animated menu button

--- ponder/responsive-design/responsive.js
+++ ponder/responsive-design/responsive.js
@@ -1,12 +1,12 @@
-let menu_btn = document.querySelector(".menu-btn");
+let menuButton = document.querySelector(".menu-btn");
 let nav = document.querySelector("nav");
 
-menu_btn.addEventListener("click", changeMenu);
+menuButton.addEventListener("click", changeMenu);
 
 function changeMenu(){
     // hides the nav on or off
     nav.classList.toggle('hide');
 
     // changes button look
-    menu_btn.classList.toggle('change');
+    menuButton.classList.toggle('change');
 }
